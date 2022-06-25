ba804a8ce12d941b805a94abc51cb959451754ad
JacobLinCool <jacoblincool@gmail.com>
2022-06-26 03:29:16 +0800

    chore: remove unused `StarportCarrier`

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { useRouter } from "vue-router";
-import { StarportCarrier } from "vue-starport";
 import Nav from "./components/Nav.vue";
 
 const router = useRouter();
@@ -15,23 +14,21 @@ if (from) {
 </script>
 
 <template>
-    <StarportCarrier>
-        <Nav />
+    <Nav />
 
-        <div class="mt-16 h-[calc(100vh-4rem)] w-full overflow-y-auto overflow-x-hidden">
-            <router-view v-slot="{ Component }">
-                <transition
-                    mode="out-in"
-                    enter-active-class="duration-200 ease-out"
-                    enter-from-class="transform opacity-0"
-                    enter-to-class="opacity-100"
-                    leave-active-class="duration-150 ease-in"
-                    leave-from-class="opacity-100"
-                    leave-to-class="transform opacity-0"
-                >
-                    <component :is="Component" />
-                </transition>
-            </router-view>
-        </div>
-    </StarportCarrier>
+    <div class="mt-16 h-[calc(100vh-4rem)] w-full overflow-y-auto overflow-x-hidden">
+        <router-view v-slot="{ Component }">
+            <transition
+                mode="out-in"
+                enter-active-class="duration-200 ease-out"
+                enter-from-class="transform opacity-0"
+                enter-to-class="opacity-100"
+                leave-active-class="duration-150 ease-in"
+                leave-from-class="opacity-100"
+                leave-to-class="transform opacity-0"
+            >
+                <component :is="Component" />
+            </transition>
+        </router-view>
+    </div>
 </template>
